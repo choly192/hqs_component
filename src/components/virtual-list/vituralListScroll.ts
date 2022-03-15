b8c1d9798be27fd10cc6d3629f731147c82ffb9a
choly192 <45955108+choly192@users.noreply.github.com>
2022-03-15 18:25:07 +0800

    modify  vituralList modify  vituralList

--- src/components/virtual-list/vituralListScroll.ts
+++ src/components/virtual-list/vituralListScroll.ts
@@ -8,7 +8,7 @@ export interface VitualListScrollPropsModal {
   container: HTMLElement;
   listSizeLimit: number;
   listItemHeight: number;
-  render: () => void;
+  render: (index: number) => void;
 }
 
 export interface DomDataCacheModal {
@@ -25,6 +25,7 @@ export class VitualListScroll {
   protected firstItemEle: HTMLElement;
   protected lastItemEle: HTMLElement;
   protected domDataCache: DomDataCacheModal;
+  protected observer: IntersectionObserver;
   constructor(protected readonly _props: VitualListScrollPropsModal) {
     this.initPropsCheck();
   }
@@ -52,5 +53,144 @@ export class VitualListScroll {
   // update cache
   protected updateDomDataCache(newData) {
     Object.assign(this.domDataCache, newData);
+  }
+
+  // 动态调整容器的padding实现滚动
+  protected adjustPadding(isScrollDown: boolean) {
+    const { container, listItemHeight, listSizeLimit } = this._props;
+    const { currentPaddingTop, currentPaddingBottom } = this.domDataCache;
+    let [newCurrentPaddingTop, newCurrentPaddingBottom] = [0, 0];
+
+    // compute padding increment
+    const paddingIncrementVal = listItemHeight * Math.floor(listSizeLimit / 2);
+
+    if (isScrollDown) {
+      newCurrentPaddingTop = currentPaddingTop + paddingIncrementVal;
+      newCurrentPaddingBottom =
+        currentPaddingBottom === 0
+          ? 0
+          : currentPaddingBottom - paddingIncrementVal;
+    } else {
+      newCurrentPaddingBottom = currentPaddingBottom + paddingIncrementVal;
+      newCurrentPaddingTop =
+        currentPaddingTop === 0 ? 0 : currentPaddingTop - paddingIncrementVal;
+    }
+
+    // 给 container 动态设置padding
+    container.style.paddingBottom = `${newCurrentPaddingBottom}px`;
+    container.style.paddingTop = `${newCurrentPaddingTop}px`;
+
+    // update cachae
+    this.updateDomDataCache({
+      currentPaddingTop: newCurrentPaddingTop,
+      currentPaddingBottom: newCurrentPaddingBottom
+    });
+  }
+
+  // compute first index
+  protected getFirstIndex(isScrollDown: boolean) {
+    const { currentIndex } = this.domDataCache;
+
+    // 以容器内元素的一半作为增量
+    const indexIncrement = Math.floor(this._props.listSizeLimit / 2);
+    let firstIndex = 0;
+    firstIndex = isScrollDown
+      ? currentIndex + indexIncrement
+      : currentIndex - indexIncrement;
+
+    if (firstIndex < 0) {
+      firstIndex = 0;
+    }
+    return firstIndex;
+  }
+
+  // IntersectionObserver 观察首尾节点是否进入/离开视窗
+  protected initIntersectionObserver() {
+    const { firstItemId, lastItemId } = this._props;
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.target.id === firstItemId && entry.isIntersecting) {
+          this.topAccurateJudgment(entry); // 上滑精准判断
+        } else if (entry.target.id === lastItemId && entry.isIntersecting) {
+          this.bottomAccurateJudgment(entry); // 下滑精准判断
+        }
+      });
+    });
+
+    this.observer.observe(this.firstItemEle);
+    this.observer.observe(this.lastItemEle);
+  }
+
+  // 上滑精准判断
+  protected topAccurateJudgment(entry) {
+    const {
+      topSentinelPreviousY,
+      topSentinelPreviousRatio
+    } = this.domDataCache;
+    const currentY = entry.boundingClientRect.top;
+    const currentRatio = entry.intersectionRatio;
+
+    if (
+      currentY > topSentinelPreviousY &&
+      currentRatio >= topSentinelPreviousRatio
+    ) {
+      const firstIndex = this.getFirstIndex(false);
+      this._props.render(firstIndex);
+      this.adjustPadding(false);
+
+      this.updateDomDataCache({
+        currentIndex: firstIndex,
+        topSentinelPreviousY: currentY,
+        topSentinelPreviousRatio: currentRatio
+      });
+    } else {
+      this.updateDomDataCache({
+        topSentinelPreviousY: currentY,
+        topSentinelPreviousRatio: currentRatio
+      });
+    }
+  }
+
+  // 下滑精准判断
+  protected bottomAccurateJudgment(entry) {
+    const {
+      bottomSentinelPreviousRatio,
+      bottomSentinelPreviousY
+    } = this.domDataCache;
+
+    const currentY = entry.boundingClientRect.top;
+    const currentRatio = entry.intersectionRatio;
+
+    if (
+      currentY < bottomSentinelPreviousY &&
+      currentRatio >= bottomSentinelPreviousRatio
+    ) {
+      const firstIndex = this.getFirstIndex(true);
+
+      this._props.render(firstIndex);
+      this.adjustPadding(true);
+
+      this.updateDomDataCache({
+        currentIndex: firstIndex,
+        bottomSentinelPreviousY: currentY,
+        bottomSentinelPreviousRatio: currentRatio
+      });
+    } else {
+      this.updateDomDataCache({
+        bottomSentinelPreviousY: currentY,
+        bottomSentinelPreviousRatio: currentRatio
+      });
+    }
+  }
+
+  // start observer
+  public startObserver() {
+    this.initIntersectionObserver();
+  }
+
+  // unObserver
+  public unObserver() {
+    this.observer.unobserve(this.firstItemEle);
+    this.observer.unobserve(this.lastItemEle);
   }
 }
